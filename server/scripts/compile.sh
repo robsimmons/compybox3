bf2492b956392b3670e1caab01ba382e914980d4
Rob Simmons <rob@lean-fro.org>
2026-05-14 15:19:13 -0400

    Give up completetly on memory usage limits — handle this at the systemd level

--- server/scripts/compile.sh
+++ server/scripts/compile.sh
@@ -18,7 +18,6 @@ LEAN_ROOT="$(lean --print-prefix)"
 SH=$(realpath $(which sh))
 SCRIPT=$(cat <<EOF
 ulimit -t 60       # 60 seconds
-# ulimit -v 16777216 # 16gb (includes mmaped things)
 ulimit -u 65536    # 65536 subprocesses spawnable (lake can use a lot here!)                                                                                        
 ulimit -f 524288   # File output size limits
 exec /lean/bin/lake build "$MODULE_NAME"
