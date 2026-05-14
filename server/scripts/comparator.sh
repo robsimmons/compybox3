2ed17cec420a1777ea697ced2381a998e7813a54
Rob Simmons <rob@lean-fro.org>
2026-05-14 14:56:23 -0400

    Just don't limit subproces spawn

--- server/scripts/comparator.sh
+++ server/scripts/comparator.sh
@@ -19,7 +19,7 @@ SH=$(realpath $(which sh))
 SCRIPT=$(cat <<EOF
 ulimit -t 60       # 60 seconds
 ulimit -v 16777216 # 16gb (includes mmaped things)
-ulimit -u 65536    # 65536 subprocesses spawnable (lake can use a lot here!)                                                                                        
+# ulimit -u 65536    # 65536 subprocesses spawnable (lake can use a lot here!)                                                                                        
 ulimit -f 524288   # File output size limits
 exec /lean/bin/lake env comparator/.lake/build/bin/comparator config.json                                                                                                                                                                          
 EOF
